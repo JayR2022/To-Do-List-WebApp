69be888d694023855f7918b54b28f6b400269076
Jay.R <jwatson2022@protonmail.com>
2022-05-17 22:04:23 +0100

    initial commit

---
+++ src/main/java/com/jr/tdl/main/service/UserService.java
@@ -0,0 +1,31 @@
+package com.jr.tdl.main.service;
+
+import org.springframework.stereotype.Service;
+
+import com.jr.tdl.main.persistence.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	private UserRepository userRepo;
+//	
+//	UserService(){
+//		super();
+//	}
+//	
+//	UserService()
+//	
+	
+	
+//	Create User
+	
+	
+//	Read User
+	
+// Update User
+	
+//	Delete User
+	
+
+
+}

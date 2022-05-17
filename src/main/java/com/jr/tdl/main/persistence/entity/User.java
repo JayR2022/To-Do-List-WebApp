7376af7de4ab676169dbd4fb3045900757135915
Jay.R <jwatson2022@protonmail.com>
2022-05-17 22:45:02 +0100

    fix: minor

--- src/main/java/com/jr/tdl/main/persistence/entity/User.java
+++ src/main/java/com/jr/tdl/main/persistence/entity/User.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 //import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+//import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -32,19 +34,23 @@ public class User {
 //	@JoinColumn(name="Card_id", nullable=false)    
 	private List<TdCard> TdCards;  
 	
+	@Size(min = 2, max = 12)
 	@Column(name = "user_name",unique = true, nullable = false)
 	String username;
 	
+	@Size(min = 6, max = 12)
 	@Column(name = "pass_word",nullable = false)
 	String password;
 	
 	@Column(name = "first_Name",nullable = false)
-	String FirstName;
+	String firstName;
 	
 	@Column(name="second_Name",nullable = false)
-	String SecondName;
+	String secondName;
 	
+
 	@Column(name="email",nullable = false)
+//	@Pattern("^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
 	String email;
 	
 }

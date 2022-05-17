9bc884d4730b19bc4fc72ff31956623ccd352738
Jay.R <jwatson2022@protonmail.com>
2022-05-17 14:15:28 +0100

    feat: entity - user, respository - user created

---
+++ src/main/java/com/jr/tdl/main/persistence/entity/User.java
@@ -0,0 +1,36 @@
+package com.jr.tdl.main.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long userId;
+	
+	@Column(unique = true, nullable = false)
+	String username;
+	
+	@Column(nullable = false)
+	String password;
+	
+	@Column(nullable = false)
+	String FirstName;
+	
+	@Column(nullable = false)
+	String SecondName;
+	
+	@Column(nullable = false)
+	String email;
+	
+}

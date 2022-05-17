d774b8149947769306c7fb74202205347c2fd735
Jay.R <jwatson2022@protonmail.com>
2022-05-17 22:44:13 +0100

    feat: create, add & delete methods implemented

--- src/main/java/com/jr/tdl/main/service/UserService.java
+++ src/main/java/com/jr/tdl/main/service/UserService.java
@@ -1,7 +1,10 @@
 package com.jr.tdl.main.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.jr.tdl.main.persistence.entity.User;
 import com.jr.tdl.main.persistence.repository.UserRepository;
 
 @Service
@@ -9,22 +12,32 @@ public class UserService {
 	
 	private UserRepository userRepo;
 //	
-//	UserService(){
-//		super();
-//	}
-//	
-//	UserService()
-//	
+	public UserService(UserRepository userRepo){
+		super();
+		this.userRepo = userRepo;
+	}
 	
 	
 //	Create User
-	
+	public User createUser(User user) {
+		this.userRepo.save(user);
+		return this.userRepo.getById(user.getId().floatValue());
+	}
 	
 //	Read User
+	public List<User> getUser(){
+		return this.userRepo.findAll();
+	}
 	
 // Update User
 	
 //	Delete User
+	public boolean deleteUser(Long id) {
+		this.userRepo.findById(id.floatValue());
+		this.userRepo.deleteById(id.floatValue());
+		return this.userRepo.existsById(id.floatValue());
+		
+	}
 	
 
 

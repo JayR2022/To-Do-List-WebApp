80d71c971048fa8e4f15359367841e91a4d9d93b
Jay.R <jwatson2022@protonmail.com>
2022-05-19 11:26:12 +0100

    adding model mapper

--- src/main/java/com/jr/tdl/main/config/AppConfig.java
+++ src/main/java/com/jr/tdl/main/config/AppConfig.java
@@ -1,5 +1,13 @@
 package com.jr.tdl.main.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
 public class AppConfig {
 
+	@Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+	
 }

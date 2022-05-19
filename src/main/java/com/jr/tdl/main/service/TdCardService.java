05260960dc0c31f74b500372679a8ac61cc5d9c1
Jay.R <jwatson2022@protonmail.com>
2022-05-19 09:38:48 +0100

    initial commit

---
+++ src/main/java/com/jr/tdl/main/service/TdCardService.java
@@ -0,0 +1,55 @@
+package com.jr.tdl.main.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.jr.tdl.main.exception.UserNotFoundException;
+import com.jr.tdl.main.persistence.entity.TdCard;
+import com.jr.tdl.main.persistence.repository.TdCardRepository;
+
+@Service
+public class TdCardService {
+	
+	private TdCardRepository cardRepo;
+	
+	
+	public TdCardService(TdCardRepository cardRepo) {
+		super();
+		this.cardRepo = cardRepo;	
+	}
+	
+	
+//	Create User
+	public TdCard createCard(TdCard card) {
+		return this.cardRepo.save(card);
+	}
+	
+//	Read User
+	public List<TdCard> getCards(){
+		return this.cardRepo.findAll();
+	}
+	
+// Update User
+	public TdCard updateUser(Long id, TdCard card) {
+		
+		Optional<TdCard> isCardPresent = Optional.of(this.cardRepo.findById(id).orElseThrow(UserNotFoundException::new));
+		TdCard cardPresent = isCardPresent.get();
+		
+		cardPresent.setContent(card.getContent());
+		cardPresent.setCreatedOn(card.getCreatedOn());
+		cardPresent.setSelected(card.getSelected());
+		return this.cardRepo.save(cardPresent);
+	}
+	
+//	Delete User
+	public boolean deleteUser(Long id) {
+		this.cardRepo.findById(id).orElseThrow(UserNotFoundException::new);
+		this.cardRepo.deleteById(id);
+		return this.cardRepo.existsById(id);
+		
+	}
+			
+
+}

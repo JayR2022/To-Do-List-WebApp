1fc8b8ca16900d5b562f2e794db1450b82c876e8
Jay.R <jwatson2022@protonmail.com>
2022-05-25 14:29:22 +0100

    TDLWA-25

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,6 @@
 // click button to create a new container
+
+
 function createCard(){
 
     let div = document.querySelector('.start-div');
@@ -7,40 +9,58 @@ function createCard(){
     divCard.style ="width: 18rem;";
     let divCardBody = document.createElement('div');
     divCardBody.className = 'card-body';
+    
+    
+    // Add Button elements
+    let divInputGroup = document.createElement('div');
+    divInputGroup.className = 'input-group mb-3';
+    let addButton = document.createElement('button');
+    addButton.className='btn btn-outline-secondary';
+    addButton.type = 'button';
+    addButton.id = 'button-addon1';
+    addButton.innerHTML = 'Add';
+    
+    // Text Box elements
+    let contentTextBox = document.createElement('input');
+    contentTextBox.type='text';
+    contentTextBox.placeholder='Type to-do item...';
+    contentTextBox.class= 'form-control';
+    contentTextBox.ariaLabel='Example text with button addon';
+    contentTextBox.ariaRoleDescription='button-addon1';
+    
+    
+
     let pCardText = document.createElement('p');
-    pCardText.textContent = 'Hello';
+    pCardText.innerText = '';
     pCardText.className = 'card-text';
     let lineBreak = document.createElement("br");
+
+    function readText(){
+            pCardText.innerText = contentTextBox.value;
+        }
+
+    function removeButtonText(){
+        divInputGroup.removeChild(addButton);
+        divInputGroup.removeChild(contentTextBox);
+    }
+    addButton.addEventListener('click',readText);
     
     div.appendChild(divCard);
     divCard.appendChild(divCardBody);
-    divCardBody.appendChild(pCardText)
+    divCardBody.appendChild(divInputGroup);
+    divInputGroup.appendChild(addButton);
+    divInputGroup.appendChild(contentTextBox);
+    divCardBody.appendChild(pCardText);
     div.appendChild(lineBreak);
+
+    addButton.addEventListener('click',removeButtonText);
+    // divInputGroup.removeChild(addButton);
 }
 
+// 
 
 
 let btn = document.querySelector('#create_button');
 btn.addEventListener('click',createCard);
 
 
-
-
-
-// btn.innerText = "Hello World";
-
-// let para = document.querySelectorAll('button');
-// for(let p of para) {
-//     p.style.color = 'blue';
-// }
-
-// console.log("Hello");
-
-// let btn1 = document.querySelector(".btn.btn-secondary");
-// console.log("Hi"+btn1);
-// btn1.addEventListener('click',printHello);
-// btn.onclick=printHello;
-
-// document.getElementById("myBtn").addEventListener("click", displayDate);
-
-// making a post request
